69258c8b3b52dd8011addcdcc5f2aa112118e443
geoffreeve <49611812+geoffreeve@users.noreply.github.com>
2021-04-15 14:41:07 +1200

    update
    03_math_gen finished
    error prevention and escape keyword added and working.

--- 03_Math_Gen.py
+++ 03_Math_Gen.py
@@ -1,5 +1,6 @@
 import random
 
+
 # This function takes the symbol selected, min num and max num to generate a question that suits the users needs.
 def generate(symbol, min, max):
     # Generates numbers between the min and max numbers inputted by user
@@ -24,7 +25,7 @@ def generate(symbol, min, max):
         answer = num_A / num_B
 
     # This print statement takes the first generated number, the symbol from the symbol list, and last generated number.
-    user_answer = int(input("{} {} {} = ".format(num_A, symbol_list[symbol-1], num_B)))
+    user_answer = number_checker("{} {} {} = ".format(num_A, symbol_list[symbol-1], num_B), 0, 100000, "Please enter a valid number.")
     
     # Checks if users answer matches programs answer, if it does then it prints correct.
     if user_answer == answer:
@@ -32,25 +33,36 @@ def generate(symbol, min, max):
     else:
         print("Incorrect")
 
+            
 # Error prevention function, checks if user input is valid.
 def number_checker(question, min, max, error):
     while True:
         try:
-            response = int(input(question))
-            if response < min or response >= max:
+            response = (input(question))
+            # If the user enters xx, the program will exit.
+            if response == "xx":
+                exit()
+            # Checks if the user has entered a response that is both higher than min and lower than max.
+            elif int(response) < min or int(response) > max:
                 print(error)
                 continue
+            # If the response is valid it will be returned
             else:
-                return response
-                break
+                return int(response)
+        # If the user enters anything invalid that hasn't been covered by if statements, it will be caught here with a error printed.
         except ValueError:
             print(error)
             continue
-            
+
 
 # Main routine
+# Short instructions
+print("***Enter 'xx' to exit the program at any time.***")
 # Ask user to select a symbol
-symbol = number_checker("1.+ 2.- 3.* 4./  ", 1, 4, "Please enter a number between 1 and 4.")
+symbol = number_checker("1.+ 2.- 3.* 4./  ", 1, 4, "Please enter a valid number.")
+# Ask user to input min range number
 min_num = number_checker("Enter minimum number: ", 1, 100000, "Please enter any number higher than 0")
+# Ask user to input max range number
 max_num = number_checker("Enter maximum number: ", min_num, 100000, "Please enter a number higher than {}".format(min_num))
+# Calls generate function to generate a equation with the given values (symbol, min-max number range).
 generate(symbol, min_num, max_num)
