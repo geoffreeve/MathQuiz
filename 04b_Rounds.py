b16cd9a2fa3334351628289e541fb8adc31233df
geoffreeve <49611812+geoffreeve@users.noreply.github.com>
2021-05-03 11:46:12 +1200

    update
    rounds 04B almost done, rounds adding on each other

--- 04b_Rounds.py
+++ 04b_Rounds.py
@@ -36,6 +36,25 @@ def generate(symbol, min, max, rounds):
         print("Incorrect")
 
             
+def selection():
+    # Ask user to select a symbol
+    symbol = number_checker("1.+ 2.- 3.* 4./  ", 1, 4, "Please enter a valid number.")
+    # Ask user to input min range number
+    min_num = number_checker("Enter minimum number: ", 1, 100000, "Please enter any number higher than 0")
+    # Ask user to input max range number
+    max_num = number_checker("Enter maximum number: ", min_num, 100000, "Please enter a number higher than {}".format(min_num))
+
+    # Asks user how many rounds they want
+    rounds = number_checker("How many rounds: ", 1, 100000, "Please enter a valid integer higher than 0")
+    print("Rounds left: {}".format(rounds))
+    while rounds > 0:
+        print(rounds)
+        # Calls generate function to generate a equation with the given values (symbol, min-max number range).
+        generate(symbol, min_num, max_num, rounds)
+        rounds -= 1
+    print("GameOver")
+
+
 # Error prevention function, checks if user input is valid.
 def number_checker(question, min, max, error):
     while True:
@@ -43,7 +62,7 @@ def number_checker(question, min, max, error):
             response = (input(question))
             # If the user enters xx, the program will exit.
             if response == "xx":
-                exit()
+                selection()
             # Checks if the user has entered a response that is both higher than min and lower than max.
             elif int(response) < min or int(response) > max:
                 print(error)
@@ -60,17 +79,4 @@ def number_checker(question, min, max, error):
 # Main routine
 # Short instructions
 print("***Enter 'xx' to exit the program at any time.***")
-# Ask user to select a symbol
-symbol = number_checker("1.+ 2.- 3.* 4./  ", 1, 4, "Please enter a valid number.")
-# Ask user to input min range number
-min_num = number_checker("Enter minimum number: ", 1, 100000, "Please enter any number higher than 0")
-# Ask user to input max range number
-max_num = number_checker("Enter maximum number: ", min_num, 100000, "Please enter a number higher than {}".format(min_num))
-
-# Asks user how many rounds they want
-rounds = number_checker("How many rounds: ", 1, 100000, "Please enter a valid integer higher than 0")
-while rounds > 0:
-    # Calls generate function to generate a equation with the given values (symbol, min-max number range).
-    generate(symbol, min_num, max_num, rounds)
-    rounds -= 1
-print("GameOver")
+selection()
