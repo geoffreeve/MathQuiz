0aaa36ba1280b22c0662f29da3e1a3f7f083909a
geoffreeve <49611812+geoffreeve@users.noreply.github.com>
2021-05-05 09:54:58 +1200

    update
    Added selection class and rounds class, need to pass a number through start class to check which mode user selected, then call following class

--- 04c_Rounds.py
+++ 04c_Rounds.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+# User selects 1 of 3 options rounds, timer, unlimited. When the button is pressed, a number will pass through a function which is a while loop, checking
+# Which mode the user selected. This will then call the class for the given mode.
 
 # This class runs when the program starts and is the main window.
 class Start:
@@ -57,45 +59,69 @@ class Start:
 
 class Selection():
     def __init__(self):
+        self.selection_box = Toplevel()
         # Main window frame
-        self.selection_frame = Frame()
-        self.selection_frame.grid()
+        self.selection_frame = Frame(self.selection_box)
+        self.selection_frame.grid(padx=20, pady=5)
 
-        # Main heading label
-        self.heading_label = Label(self.selection_frame, text="Selection", font='arial 30 bold')
+        # Main heading label (row 0)
+        self.heading_label = Label(self.selection_frame, text="Select", font='arial 27 bold')
         self.heading_label.grid()
+
+        # Buttons frame
+        self.buttons_frame = Frame(self.selection_box)
+        self.buttons_frame.grid(padx=15, pady=15)
+
+        # Addition button (row 1, column 0)
+        self.addition_button = Button(self.buttons_frame, text="+", font='arial 12', width=3, command=lambda:Rounds(), selection)
+        self.addition_button.grid(row=1, column=0)
+
+        # Subtraction button (row 1, column 1)
+        self.subtraction_button = Button(self.buttons_frame, text='-', font='arial 12', width=3)
+        self.subtraction_button.grid(row=1, column=1, padx=5)
+
+        # Multiplication button (row 1, column 2)
+        self.multiplication_button = Button(self.buttons_frame, text='x', font='arial 12', width=3)
+        self.multiplication_button.grid(row=1, column=2, padx=5)
+
+        # Division button (row 1, column 3)
+        self.division_button = Button(self.buttons_frame, text='/', font='arial 12', width=3)
+        self.division_button.grid(row=1, column=3)
 
 
 
 # Rounds class
 class Rounds:
     def __init__(self):
+        # Creates new window called rounds_box
+        self.rounds_box = Toplevel()
+
         # Main window frame
-        self.rounds_frame = Frame(padx=20, pady=5)
+        self.rounds_frame = Frame(self.rounds_box, padx=20, pady=5)
         self.rounds_frame.grid()
 
-        # Heading label (row 1)
+        # Heading label (row 0)
         self.heading_label = Label(self.rounds_frame, text="Rounds", font='arial 30 bold')
         self.heading_label.grid(padx=10, pady=10)
 
-        # Question label (row 2)
+        # Question label (row 1)
         self.question_label = Label(self.rounds_frame, text="How many rounds?", font='arial 15 italic')
-        self.question_label.grid(row=2, pady=10)
+        self.question_label.grid(row=1, pady=10)
 
-        # Question entry (row 3)
+        # Question entry (row 2)
         self.question_entry = Entry(self.rounds_frame)
-        self.question_entry.grid(row=3)
+        self.question_entry.grid(row=2)
 
         # Buttons frame
-        self.buttons_frame = Frame()
+        self.buttons_frame = Frame(self.rounds_box)
         self.buttons_frame.grid()
 
-        # Back button
+        # Back button (row 3, column 0)
         self.back_button = Button(self.buttons_frame, text="Back", fg='white', bg='grey', font='arial 12')
-        self.back_button.grid(row=4, pady=10, padx=5)
-        # Enter button
+        self.back_button.grid(row=3, pady=10, padx=5)
+        # Enter button (row 3, column 1)
         self.enter_button = Button(self.buttons_frame, text="Enter", fg='white', bg='grey', font='arial 12')
-        self.enter_button.grid(row=4, column=1)
+        self.enter_button.grid(row=3, column=1)
 
 
 
